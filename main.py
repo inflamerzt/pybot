ed24e3d49b1d5c2076e33f73f71b55e89f4b104c
inflamerzt <inflamerzt@users.noreply.github.com>
2024-04-03 18:10:32 +0300

    update

--- main.py
+++ main.py
@@ -7,21 +7,28 @@ import pyautogui
 
 
 if __name__ == "__main__":
-    im,window_rect = capture()
-    print(window_rect)
+    imgw = capture()
 
-    if im !=None:
+    if imgw !=None:
+        im,window_rect = imgw
+        print(window_rect)
         print(match('templates/sadovod.png',im))
         print(match('templates/btn_cross.png',im))
         #print(match('templates/btn_close.png',im,True))
 
         out = match('templates/btn_close.png',im)
-        if out[0] > 0.85:
+        if out[0] > 0.95:
             pyautogui.moveTo(window_rect[0]+out[1][0]+int(out[2][0]/2), window_rect[1]+out[1][1]+int(out[2][1]/2))
             pyautogui.leftClick()
   
     else:
         print("Bluestacks window not found")
     
+
+    while True:
+        #command = input("type exit to exit:")
+        command = "exit"
+        if command == "exit":
+            break
 
     #time.sleep(10)
