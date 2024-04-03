af9a8cdc02880a811a467a5c2c68153394de3b77
inflamerzt <inflamerzt@users.noreply.github.com>
2024-04-03 12:01:28 +0300

    cleanup and test

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import numpy as np
 if __name__ == "__main__":
     im = capture()
     if im !=None:
-        #template = cv2.imread('btn_close.png', cv2.IMREAD_COLOR)
-        template = cv2.imread('sadovod.png', cv2.IMREAD_COLOR)
+        #template = cv2.imread('templates/btn_close.png', cv2.IMREAD_COLOR)
+        template = cv2.imread('templates/sadovod.png', cv2.IMREAD_COLOR)
         tc,tw,th = template.shape[::-1]
 
         #template = cv2.cvtColor(template, cv2.COLOR_)
@@ -23,13 +23,24 @@ if __name__ == "__main__":
         
 
         img = cv2.cvtColor(np.asarray(im), cv2.COLOR_RGBA2RGB)
+        #cv2.imshow("img",img)
         
+
+
+        #https://docs.opencv.org/4.x/d4/dc6/tutorial_py_template_matching.html
         res = cv2.matchTemplate(img,template,cv2.TM_CCOEFF_NORMED)
-        threshold = 0.8
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+        print("min="+str(min_loc)+"("+str(min_val)+");max="+str(max_loc)+"("+str(max_val)+");")
+
+        cv2.rectangle(img, max_loc, (max_loc[0] + tw, max_loc[1] + th), (255,0,0), 3)
+
+        threshold = 0.9
         loc = np.where( res >= threshold)
 
         for pt in zip(*loc[::-1]):
-            cv2.rectangle(img, pt, (pt[0] + tw, pt[1] + th), (0,0,255), 2)
+            cv2.rectangle(img, pt, (pt[0] + tw, pt[1] + th), (255,255,255), 1)
+            print(pt)
         
         cv2.imshow("array",np.asarray(img))
         cv2.waitKey()
